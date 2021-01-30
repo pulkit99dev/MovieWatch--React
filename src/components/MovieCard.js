05a86a5216a9a8f65396089d8de6252b764e14d6
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-30 19:14:04 +0530

    Adding styling & UI for components-MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,8 +3,11 @@ import React from 'react';
 class MovieCard extends React.Component {
     render(){
         return (
-            <div>
-                MovieCard
+            <div className='movie-card'>
+                <div className='left'>
+                    <img alt='movie poster' src={} />
+                </div>
+                <div className='right'></div>
             </div>
         );
 }
