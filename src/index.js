959262c05320d89ad98a64036b8e6aa7be350cd2
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-31 00:27:43 +0530

    Adding Reducers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import { createStore } from 'redux';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('STATE', store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
