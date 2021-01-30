20c070522000f93c6bd5229848059b33970d44f1
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-30 19:21:46 +0530

    Adding data to Movie-Card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 class MovieCard extends React.Component {
     render(){
-        return (
-            <div className='movie-card'>
-                <div className='left'>
-                    <img alt='movie poster' src={} />
+        const {movie} = this.props;
+            return (
+                <div className='movie-card'>
+                    <div className='left'>
+                        <img alt='movie poster' src={movie.Poster} />
+                    </div>
+                    <div className='right'>
+                        <div className='title'>{movie.Title}</div>
+                        <div className='plot'>{movie.Plot}</div>
+                        <div className='footer'>
+                            <div className='rating'>{movie.imdbRating}</div>
+                            <button className='favourite-btn'>Favourite</button>
+                        </div>
+                    </div>
                 </div>
-                <div className='right'></div>
-            </div>
-        );
+            );
 }
 }
 
